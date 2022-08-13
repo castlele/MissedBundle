75f76a44994767f167c3a040ab9226382ea3dd8c
Nikita Semenov <castlelecs@gmail.com>
2022-08-13 22:45:16 +0300

    fix: initial reorganization

---
+++ src/main/java/com/castlelecs/missedbundle/MissedBundle.java
@@ -0,0 +1,10 @@
+package com.castlelecs.missedbundle;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MissedBundle.MODID)
+public class MissedBundle
+{
+    public static final String MODID = "missedbundle";
+
+}
